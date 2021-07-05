44fa92344d95c7f0c781aa0dbbb4c3ec6455495d
Muhammad Jawad <jawad.topdot@gmail.com>
2021-07-05 11:20:38 +0500

    Resolved route issue

--- src/routes/media.php
+++ src/routes/media.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('media')->name('media.')->namespace('Topdot\Media\App\Http\Controllers')->group(function(){
 	Route::get('{media}', 'MediaController@show')->name('show');
-	Route::post('/', ['MediaController@store'])->name('store');
-	Route::delete('/', ['MediaController@removeTemp'])->name('temp.delete');
-	Route::delete('{media}', ['MediaController@destroy'])->name('destroy');
+	Route::post('/', 'MediaController@store')->name('store');
+	Route::delete('/', 'MediaController@removeTemp')->name('temp.delete');
+	Route::delete('{media}', 'MediaController@destroy')->name('destroy');
 });
